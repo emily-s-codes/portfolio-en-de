0142c29cda4edd0e82bdce84053d07c235c38578
emilysupercode <sheil.emily.sc@gmail.com>
2023-01-23 14:43:45 +0100

    small styles, scroll to top

--- portfolio-frontend/src/App.js
+++ portfolio-frontend/src/App.js
@@ -9,6 +9,7 @@ import EmailMe from "./components/emailMe/EmailMe"
 import { useState } from 'react';
 import Scroll from './components/scroll/Scroll';
 import Datenschutz from './pages/Datenschutz';
+import ScrollToTop from './components/autoScrollTop/ScrollToTop';
 
 function App() {
   const [english, setEnglish] = useState(false)
@@ -16,16 +17,18 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Header english={english} setEnglish={setEnglish} />
-        <Routes>
-          <Route path={"/"} element={<Home english={english} />} />
-          <Route path={"/projects/:id"} element={<ProjectPage english={english} />} />
-          <Route path={"/impressum"} element={<Impressum english={english} />} />
-          <Route path={"/datenschutz"} element={<Datenschutz english={english} />} />
-        </Routes>
-        <Scroll />
-        <EmailMe english={english} />
-        <Footer english={english} />
+        <ScrollToTop>
+          <Header english={english} setEnglish={setEnglish} />
+          <Routes>
+            <Route path={"/"} element={<Home english={english} />} />
+            <Route path={"/projects/:id"} element={<ProjectPage english={english} />} />
+            <Route path={"/impressum"} element={<Impressum english={english} />} />
+            <Route path={"/datenschutz"} element={<Datenschutz english={english} />} />
+          </Routes>
+          <Scroll />
+          <EmailMe english={english} />
+          <Footer english={english} />
+        </ScrollToTop>
       </Router>
     </div>
   );
--- portfolio-frontend/src/pages/Datenschutz.jsx
+++ portfolio-frontend/src/pages/Datenschutz.jsx
@@ -11,19 +11,7 @@ const Datenschutz = ({ english }) => {
                 <p>{english ? "When you visit and browse this site, none of your data is saved or tracked. In the event you voluntarily submit your name, email address and a message in the form and click submit, the data that you transmit will used only to send an email to me and as a convenience, a copy of your email to you. I do not save this information." : "Wenn Sie diese Seite besuchen und durchblättern, werden keine Ihrer Daten gespeichert oder nachverfolgt. Für den Fall, dass Sie freiwillig Ihren Namen, Ihre E-Mail-Adresse und eine Nachricht in das Formular eingeben und auf 'Senden' klicken, werden die von Ihnen übermittelten Daten nur dazu verwendet, eine E-Mail an mich zu senden und als Annehmlichkeit eine Kopie Ihrer E-Mail an Sie. Ich speichere diese Informationen nicht."} </p>
             </section>
             <section>
-                <h3>{english ? "Data subject rights" : "Betroffenenrechte"}</h3>
-                <p>{english ? "" : ""} </p>
-            </section>
-            <section>
-                <h3>{english ? "Third-party plugins" : "Plugins von Drittanbietenden"}</h3>
-                <p>{english ? "Easy, I don't use any." : "Ganz einfach, ich verwende keine."} </p>
-            </section>
-            <section>
-                <h3>{english ? "Commissioned service providers used" : "eingesetzte Auftragsdienstleistende"}</h3>
-                <p>{english ? "Easy, I don't use any." : "Ganz einfach, ich verwende keine."} </p>
-            </section>
-            <section>
-                <h3>Cookies</h3>
+                <h3>{english ? "Third-party plugins, Commissioned service providers, and Cookies" : "Plugins von Drittanbietenden, eingesetzte Auftragsdienstleistende, und Cookies"}</h3>
                 <p>{english ? "Easy, I don't use any." : "Ganz einfach, ich verwende keine."} </p>
             </section>
         </main>
